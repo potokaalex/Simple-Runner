ac8d660c4a128ff91c36e738c50a1eff8439289c
Kot-Alenya <nevskial686@gmail.com>
2022-11-06 20:44:17 +0300

    Change the killing condition
    added the ability to dynamically change the conditions of death of the character

--- Assets/Scripts/Entity/Character/Character.cs
+++ Assets/Scripts/Entity/Character/Character.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 using System;
 
-public class Character : PhysicalEntity // only data and interfaceS for them / 
+public class Character : PhysicalEntity // only data and interfaceS for them //
 {
     [SerializeField] private float health;
 
     public event Action IsKilled;
 
+    public bool IsTimeToKill => KillCondition.Invoke();
+
+    private Func<bool> KillCondition
+    {
+        get => KillCondition == null ? () => health <= 0 : KillCondition;
+        set { }
+    }
+
+    // Im not sure, is this should be here ? 
     private float score;
-
-
     public float GetScore() => score;
     public float SetScore(float value) => score = value;
+    //
 
     public float GetHealth() => health;
     public float ChangeHealth(float value) => health += value;
 
-    public bool IsTimeToKill => health > 0 ? false : true;
+    public void ChangeKillCondition(Func<bool> newCondition) => KillCondition = newCondition;
     public void Kill() => IsKilled?.Invoke();
 }
