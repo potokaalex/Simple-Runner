cffe5ab558aac6da260cdee50c4566e5ac869abf
Kot-Alenya <nevskial686@gmail.com>
2023-01-20 20:05:52 +0300

    Event system
    An event system has been created !

---
+++ Assets/Client/Scripts/Event/EventSystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecs.Systems
+{
+    public class EventSystem : IFixedUpdateSystem
+    {
+        private HashSet<IEvent> _events = new();
+
+        public void FixedUpdate(float deltaTime)
+        {
+            if (_events.Count < 1)
+                return;
+
+            _events.Clear();
+        }
+
+        public HashSet<IEvent> GetEvents() => _events;
+
+        public bool TryAddEvent(IEvent eventEntity) => _events.Add(eventEntity);
+    }
+}
